de410af6996f67be08daca467c42384945ee03ae
iKitzu <kennethsantiagoc@gmail.com>
2024-06-20 15:14:07 -0500

    feat: :sparkles: Dia 2 MySql 2 "La venganza de Pedro"

---
+++ Dia 02/tablas.sql
@@ -0,0 +1,20 @@
+create database Dia02_Actividad_CeballosKenneth;
+
+use Dia02_Actividad_CeballosKenneth;
+
+CREATE TABLE departamento (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+presupuesto DOUBLE UNSIGNED NOT NULL,
+gastos DOUBLE UNSIGNED NOT NULL
+); 
+
+CREATE TABLE empleado (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+nif VARCHAR(9) NOT NULL UNIQUE,
+nombre VARCHAR(100) NOT NULL,
+apellido1 VARCHAR(100) NOT NULL,
+apellido2 VARCHAR(100),
+id_departamento INT UNSIGNED,
+FOREIGN KEY (id_departamento) REFERENCES departamento(id)
+);
